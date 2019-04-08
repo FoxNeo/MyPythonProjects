869e4f78c49d688182b501bc0fa041a2c203522b
FoxNeo <mcaceres@uni-bremen.de>
2019-04-08 21:38:51 +0200

    Add conjuntos

---
+++ src/basics/conjuntos/demo_conjuntos.py
@@ -0,0 +1,9 @@
+colores = {"yellow", "green", "black"}
+print(colores)
+for color in colores:
+    print(color)
+print(len(colores))
+colores.add("white")
+print(colores)
+colores.remove("green")
+print(colores)
