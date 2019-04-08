605e7539ada25109655429b5495c567d0642d5ab
FoxNeo <mcaceres@uni-bremen.de>
2019-04-08 22:03:13 +0200

    Add for loop

---
+++ src/basics/loops/demo_for.py
@@ -0,0 +1,10 @@
+countries = ["USA", "Spain", "France", "Canada"]
+for country in countries:
+    print(country)
+
+data = "Hello from python"
+for out in data:
+    print(out)
+
+for numero in range(8):
+    print(numero)
