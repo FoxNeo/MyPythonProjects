87081b0199163e6a89b5f9509269b81c65710b93
FoxNeo <mcaceres@uni-bremen.de>
2019-04-14 15:10:29 +0200

    Add frame component

---
+++ src/GUI/frame.py
@@ -0,0 +1,11 @@
+import tkinter
+
+raiz = tkinter.Tk()
+raiz.title("Software in Python")
+
+# frame component
+frame = tkinter.Frame()
+frame.config(bg="blue", width=400, height=300)
+frame.pack()
+
+raiz.mainloop()
