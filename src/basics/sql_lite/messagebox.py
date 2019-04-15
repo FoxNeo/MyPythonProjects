349d40905a452ec4f14882bdea09ca1634c95cc7
foxneo <mcaceres@uni-bremen.de>
2019-04-15 09:20:39 +0200

    Add  messagebox

---
+++ src/basics/sql_lite/messagebox.py
@@ -0,0 +1,14 @@
+import tkinter
+from tkinter import messagebox
+
+raiz = tkinter.Tk()
+raiz.title("Mi programa")
+
+def avisar():
+    tkinter.messagebox.showinfo("Titulo", "Mensaje con la información")
+
+# component
+boton = tkinter.Button(raiz, text="Pulsar para aviso", command=avisar)
+boton.pack()
+
+raiz.mainloop()
