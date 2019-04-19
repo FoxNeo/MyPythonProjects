75ddbfa1adbdefa5dcb07afd670350e4ddde1541
foxneo <mcaceres@uni-bremen.de>
2019-04-19 16:15:22 +0200

    Add arrays function

---
+++ src/NumpyModule/arraysfunction.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+array = np.arange(5)
+print(array)
+
+result = np.sqrt(array)
+print(result)
+
+random = np.random.rand(5)
+print(random)
+
+lista = [5,6,7,8,9]
+array2 = np.array(lista)
+
+sum = np.add(array, array2)
+print(sum)
+
+max = np.maximum(array, array2)
+print(max)
