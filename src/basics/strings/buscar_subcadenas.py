6c157f662f93ec086e22d151741528f54689428d
Miguel Caceres <m.caceres@mailbox.org>
2025-02-09 10:36:35 +0100

    Add Python strings operations

---
+++ src/basics/strings/buscar_subcadenas.py
@@ -0,0 +1,4 @@
+# Buscar subcadenas
+cadena = 'Test python'
+indice = cadena.find('python')
+print(f'Indice de la subcadena mundo: {indice}')
---
+++ src/basics/strings/subcadenas.py
@@ -0,0 +1,10 @@
+# Manejo de subcadenas
+cadena = 'Test python'
+
+# Obtenemos la subcadena de Test
+subcadena = cadena[0:4]
+print(f'Subcadena de: {subcadena}')
+
+# Obtener la subcadena de python
+subcadena_python = cadena[5:11]
+print(f'subcadena de python: {subcadena_python}')
